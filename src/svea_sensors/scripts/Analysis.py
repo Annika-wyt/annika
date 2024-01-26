319216b3d51ced15bbe168f70aa1454f55ed8b28
Annika-wyt <yintingwong@gmail.com>
2024-01-26 10:10:56 +0100

    added plots and updated analysis.py

--- src/svea_sensors/scripts/Analysis.py
+++ src/svea_sensors/scripts/Analysis.py
@@ -13,7 +13,8 @@ import os
 
 from nav_msgs.msg import Odometry
 
-BAGNUMBER = 7
+BAGNUMBER = 5
+SAVEPLOT = False
 
 BagPaths = {
     1: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-22/working_epnp_with_EKF.bag',
@@ -22,27 +23,23 @@ BagPaths = {
     4: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-25/epnp_alone_4_landmarks.bag',
     5: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-25/iterative_pnp_alone.bag',
     6: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-25/iterative_pnp_with_ekf.bag',
-    7: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-25/p3p_alone.bag'
+    7: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-25/p3p_alone.bag',
+    8: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-26/epnp_alone_6_landmark.bag',
+    9: '/home/annika/ITRL/kth_thesis/rosbag/2024-01-26/epnp_with_ekf_6_landmark.bag'
     }
 
 Bag = BagPaths[BAGNUMBER]
-if BAGNUMBER == 5 or BAGNUMBER == 6:
-    ARUCOLIST = [0, 10, 11, 12, 13, 14]
-elif BAGNUMBER == 4:
-    ARUCOLIST = [10, 11, 12, 14]
-elif BAGNUMBER == 7:
-    ARUCOLIST = [11, 12, 14]
-else:
-    ARUCOLIST = [10, 11, 12, 13, 14]
 
 class Analysis:
     def __init__(self):
+        global ARUCOLIST
         rospy.init_node("Analysis")
         rospy.set_param('/use_sim_time', True)
         self.buffer = tf2_ros.Buffer(rospy.Duration(10))
         self.listener = tf2_ros.TransformListener(self.buffer)
         self.br = tf2_ros.TransformBroadcaster()
-        if BAGNUMBER == 5 or BAGNUMBER == 6:
+        if BAGNUMBER == 5 or BAGNUMBER == 6 or BAGNUMBER == 8 or BAGNUMBER == 9:
+            ARUCOLIST = [0, 10, 11, 12, 13, 14]
             self.Aruco = {
                 0: [],
                 10 : [],
@@ -52,6 +49,7 @@ class Analysis:
                 14 : []
             }
         elif BAGNUMBER == 4:
+            ARUCOLIST = [10, 11, 12, 14]
             self.Aruco = {
                 10 : [],
                 11 : [],
@@ -59,12 +57,14 @@ class Analysis:
                 14 : []
             }
         elif BAGNUMBER == 7:
+            ARUCOLIST = [11, 12, 14]
             self.Aruco = {
                 11 : [],
                 12 : [],
                 14 : []
             }
         else:
+            ARUCOLIST = [10, 11, 12, 13, 14]
             self.Aruco = {
                 10 : [],
                 11 : [],
@@ -98,7 +98,7 @@ class Analysis:
             self.END = True
 
     def getTransform(self):
-        rate = rospy.Rate(10)  # 10 Hz
+        global ARUCOLIST
         while not self.END:
             try:
                 stateDiff = self.buffer.lookup_transform("svea5", "base_link", rospy.Time.now())
@@ -106,15 +106,15 @@ class Analysis:
                     self.start_time = stateDiff.header.stamp
                 self.stateDiff.append([stateDiff.transform.translation.x, stateDiff.transform.translation.y, stateDiff.transform.translation.z, (stateDiff.header.stamp - self.start_time).to_sec()])
             except Exception as e:
-                rospy.logerr(f'At State: {e}')
+                # rospy.logerr(f'At State: {e}')
                 pass
             for id in ARUCOLIST: 
                 try:
                     trans1 = self.buffer.lookup_transform("MapAruco" + str(id), "arucoCamera" + str(id), rospy.Time.now())
                     self.Aruco[id].append([trans1.transform.translation.x, trans1.transform.translation.y, trans1.transform.translation.z, (trans1.header.stamp - self.start_time).to_sec()])
                 except Exception as e:
-                    rospy.logerr(f'At Aruco: {e}')
-                    # pass
+                    # rospy.logerr(f'At Aruco: {e}')
+                    pass
 
     def save_to_file(self, filename):
         """Save the ArUco data to a text file."""
@@ -125,7 +125,6 @@ class Analysis:
         plt.plot(self.stateDiff[:,3], self.stateDiff[:,2], color='b', linestyle='-', label=f'State - z')
         for signal_time in self.P3P_Measurement:
             plt.axvline(x=signal_time, color='c', alpha = 0.2, linestyle='-')
-
         # plt.axhline(y=np.median(self.stateDiff[:, 0]), color='r', linestyle='--', label=f'Median x - {round(np.median(self.stateDiff[:, 0]), 3)}')
         # plt.axhline(y=np.median(self.stateDiff[:, 1]), color='g', linestyle='--', label=f'Median y - {round(np.median(self.stateDiff[:, 1]), 3)}')
         # plt.axhline(y=np.median(self.stateDiff[:, 2]), color='b', linestyle='--', label=f'Median z - {round(np.median(self.stateDiff[:, 2]), 3)}')
@@ -134,15 +133,13 @@ class Analysis:
         plt.xlabel('Time (in second)')
         plt.ylabel('Error (in m)')
         plt.ylim(-0.1, 1)
-        plt.grid()
-        # plt.xlim(0, len(dataArray))
-        plt.savefig('/home/annika/ITRL/kth_thesis/plots/' + os.path.splitext(os.path.basename(Bag))[0] + '_stateDiff.png', format='png', dpi=300, bbox_inches='tight')
+        plt.grid(which = "both")
+        plt.minorticks_on()
+        if SAVEPLOT:
+            plt.savefig('/home/annika/ITRL/kth_thesis/plots/' + os.path.splitext(os.path.basename(Bag))[0] + '_stateDiff.png', format='png', dpi=300, bbox_inches='tight')
         
         for aruco_id, data in self.Aruco.items():
             plt.figure(figsize=(10, 3))
-              # Convert data list to a string of columns
-                # data_str = ', '.join(map(str, data))
-                # file.write(f'{data_str}\n')
             dataArray = np.array(data)
             plt.plot(dataArray[:,3], dataArray[:,0], color='r', label=f'Aruco {aruco_id} - x')
             plt.plot(dataArray[:,3], dataArray[:,1], color='g', label=f'Aruco {aruco_id} - y')
@@ -154,9 +151,9 @@ class Analysis:
             plt.title("Error between MOCAP and Camera Detection")
             plt.xlabel('Time (in second)')
             plt.ylabel('Error (in m)')
-            # plt.xlim(0, len(dataArray))
             plt.ylim(-3, 3)
-            plt.savefig('/home/annika/ITRL/kth_thesis/plots/' + os.path.splitext(os.path.basename(Bag))[0] + '_Aruco' + str(aruco_id) + '.png', format='png', dpi=300, bbox_inches='tight')
+            if SAVEPLOT:
+                plt.savefig('/home/annika/ITRL/kth_thesis/plots/' + os.path.splitext(os.path.basename(Bag))[0] + '_Aruco' + str(aruco_id) + '.png', format='png', dpi=300, bbox_inches='tight')
         plt.show()
 
 
